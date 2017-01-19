0b9f041dd9fc5e30fa83e54a2214c493408f70b8
mmlamare <mmlamare@gmail.com>
2017-01-19 15:31:27 -0500

    Added USARTDebug.c

---
+++ Lab1/Release/USARTDebug.d
@@ -0,0 +1,46 @@
+USARTDebug.o USARTDebug.o: ../USARTDebug.c \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/RBELib.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/ADC.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/DAC.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/Debug.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/motors.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/USARTDebug.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/timer.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/Periph.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/pot.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/PID.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/reg_structs.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/ports.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/SPI.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/SetServo.h \
+  C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/SlaveSelects.h
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/RBELib.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/ADC.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/DAC.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/Debug.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/motors.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/USARTDebug.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/timer.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/Periph.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/pot.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/PID.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/reg_structs.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/ports.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/SPI.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/SetServo.h:
+
+C:\Users\Matt\git\rbelib1\RBELib\include/RBELib/SlaveSelects.h:
--- Lab1/Release/subdir.mk
+++ Lab1/Release/subdir.mk
@@ -4,12 +4,15 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../USARTDebug.c \
 ../main.c 
 
 OBJS += \
+./USARTDebug.o \
 ./main.o 
 
 C_DEPS += \
+./USARTDebug.d \
 ./main.d 
 
 
@@ -17,7 +20,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR Compiler'
-	avr-gcc -I"C:\Users\Matt\git\rbelib\RBELib\include" -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega644p -DF_CPU=18432000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	avr-gcc -I"C:\Users\Matt\git\rbelib1\RBELib\include" -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega644p -DF_CPU=18432000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
