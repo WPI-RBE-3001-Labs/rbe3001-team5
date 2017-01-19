026b800d8bb14872b820e9febd260f0023e30d90
mmlamare <mmlamare@gmail.com>
2017-01-19 15:24:19 -0500

    Added lab 1

---
+++ Lab1/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 17, 2017
+ *      Author: Matt
+ */
+#include "RBELib/RBELib.h"
+
+void part1();
+
+int main(){
+	//Interchange the correct part of the lab
+	part1();
+	return 1;
+} /* End main */
+
+//This does the first part of the lab
+void part1(){
+
+}
