c7da8a1cbdbb3f43a97b13dc7d3425f763e43511
mmlamare <mmlamare@gmail.com>
2017-01-19 19:46:12 -0500

    I don't know if I should commit this shit, but better safe than sorry

---
+++ MLamarePrelab1/Release/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../USARTDebug.c \
+../main.c 
+
+OBJS += \
+./USARTDebug.o \
+./main.o 
+
+C_DEPS += \
+./USARTDebug.d \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -I"C:\Users\Matt\git\rbelib1\RBELib\include" -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega644p -DF_CPU=18432000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
