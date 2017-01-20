7e6cb83a66d83fdbcac9a050cf8e071fedaaf911
mmlamare <mmlamare@gmail.com>
2017-01-19 19:44:28 -0500

    Initial commit

---
+++ MLamarePrelab1/main.c
@@ -0,0 +1,30 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 17, 2017
+ *      Author: Matt
+ */
+#include "RBELib/RBELib.h"
+
+void part1();
+
+int main(){
+	//Interchange the correct part of the Pre-lab
+	part1();
+	return 1;
+} /* End main */
+
+//This does the first part of the Pre-lab
+void part1(){
+
+}
+
+//This does the second part of the Pre-lab
+void part2(){
+
+}
+
+//This does the third part of the Pre-lab
+void part3(){
+
+}
