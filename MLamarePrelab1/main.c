e16ee9d3f9de46d8e65bd61763de59800e6d1c32
mmlamare <mmlamare@gmail.com>
2017-01-20 16:41:15 -0500

    More work on my prelab

--- MLamarePrelab1/main.c
+++ MLamarePrelab1/main.c
@@ -14,17 +14,18 @@ void WDT_off();
 int main(){
 	//Common setup code here:
 	WDT_off(); //Disable watchdog timer
-	DDRA |= 0xFF; //configure port A as output for LED's
-	DDRB &= 0x00; //configure port B as input
 
 	//Interchange the correct part of the Pre-lab
 	part5();
-	return 1;
+	//should never get here but if it does: error
+	return 0;
 } /* End main */
 
 //This does the first code part of the Pre-lab
 void part5(){
 	volatile unsigned char buttonstate;
+	DDRB |= 0xFF; //configure port A as output for LED's
+	DDRA &= 0x00; //configure port B as input
 
 	while(1){
 		buttonstate = PORTA; //reads buttons
@@ -34,12 +35,35 @@ void part5(){
 
 //This does the second code part of the Pre-lab
 void part6(){
+	debugUSARTInit(DEFAULT_BAUD); //112500
+	unsigned char package = 0;
 
+	while(1){
+		package = getCharDebug(); //recieve a character
+		//only transmit when the chip has something to transmit
+		if(package){
+			putCharDebug(package);
+		}
+	}
 }
 
 //This does the third code part of the Pre-lab
 void part7(){
+	unsigned char state = 0;
+	DDRBbits._P4 = OUTPUT;
 
+	while(1){
+		switch(state){
+		case 0:
+			//the is the off case
+			PINBbits._P4 = 0;
+			break;
+		case 1:
+			//this is the on case
+			PINBbits._P4 = 1;
+			break;
+		}
+	}
 }
 
 void WDT_off(){
