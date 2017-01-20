e66c6ff82ea5e35c9d3eeb4946bf74cccff008b5
mmlamare <mmlamare@gmail.com>
2017-01-20 16:15:47 -0500

    Added watchdog timer disable function to main source file

--- Lab1/main.c
+++ Lab1/main.c
@@ -7,8 +7,13 @@
 #include "RBELib/RBELib.h"
 
 void part1();
+void part2();
+void WDT_off();
 
 int main(){
+	//Common setup here:
+	WDT_off();
+
 	//Interchange the correct part of the lab
 	part1();
 	return 1;
@@ -22,4 +27,17 @@ void part1(){
 //This does the second part of the lab
 void part2(){
 
+}
+
+void WDT_off(){
+	__disable_interrupt();
+	__watchdog_reset();
+	/* Clear WDRF in MCUSR */
+	MCUSR &= ~(1<<WDRF);
+	/* Write logical one to WDCE and WDE */
+	/* Keep old prescaler setting to prevent unintentional time-out */
+	WDTCSR |= (1<<WDCE) | (1<<WDE);
+	/*Turn off WDT */
+	WDTCSR = 0x00;
+	__enable_interrupt();
 }
