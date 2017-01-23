2dbb623dc100fad63c1db93daf9295b38fbaceff
mmlamare <mmlamare@gmail.com>
2017-01-23 12:22:57 -0500

    Initial commit for ACD source file

---
+++ Lab1/ADC.c
@@ -0,0 +1,65 @@
+/*
+ * ADC.c
+ *
+ *  Created on: Jan 23, 2017
+ *      Author: Group 5
+ */
+
+#include "RBELib/RBELib.h"
+
+/**
+ * @brief Initializes the ADC and make one channel active.
+ * You can choose to use either interrupts or polling to read
+ * the desired channel.
+ *
+ * @param channel The ADC channel to initialize.
+ *
+ * @todo Create the corresponding function to initialize the ADC
+ * using the channel parameter.
+ */
+void initADC(int channel){
+
+
+}
+
+/**
+ * @brief Disables ADC functionality and clears any saved values (globals).
+ *
+ * @param channel  The ADC channel to disable.
+ *
+ * @todo Create the corresponding function to clear the last ADC
+ * calculation register and disconnect the input to the ADC if desired.
+ */
+void clearADC(int channel){
+
+
+}
+
+/**
+ * @brief Run a conversion on and get the analog value from one ADC
+ * channel if using polling.
+ *
+ * @param channel  The ADC channel to run a conversion on.
+ * @return adcVal The 8-10 bit value returned by the ADC
+ * conversion.  The precision depends on your settings and
+ * how much accuracy you desire.
+ *
+ * @todo Create the corresponding function to obtain the value of the
+ * last calculation if you are using polling.
+ */
+unsigned short getADC(int channel){
+	return 0;
+}
+
+/**
+ * @brief Change the channel the ADC is sampling if using interrupts.
+ *
+ * @param channel  The ADC channel to switch to.
+ *
+ * @todo Create a way to switch ADC channels if you are using interrupts.
+ */
+void changeADC(int channel){
+
+
+}
+
