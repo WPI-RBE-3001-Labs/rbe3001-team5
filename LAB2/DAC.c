aa89a3801c3ff428805507cdbc37da26c2b33fe2
mmlamare <mmlamare@gmail.com>
2017-01-28 13:54:55 -0500

    initial commit

---
+++ LAB2/DAC.c
@@ -0,0 +1,22 @@
+/*
+ * DAC.c
+ *
+ *  Created on: Jan 28, 2017
+ *      Author: Matt
+ */
+
+#include "RBELib/RBELib.h"
+
+/**
+ * @brief Set the DAC to the given value on the chosen channel.
+ * @param  DACn The channel that you want to set.
+ * @param SPIVal The value you want to set it to.
+ *
+ * @todo Make the function that is able to set the DAC to a given value
+ * from 0 - 4095.
+ */
+void setDAC(int DACn, int SPIVal){
+
+}
+
+
