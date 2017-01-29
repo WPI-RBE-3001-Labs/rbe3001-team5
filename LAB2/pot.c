609b9231c0df0a7f6c5164b668cb0b80d16ca04c
mmlamare <mmlamare@gmail.com>
2017-01-29 11:07:05 -0500

    initial commit

---
+++ LAB2/pot.c
@@ -0,0 +1,31 @@
+/*
+ * pot.c
+ *
+ *  Created on: Jan 29, 2017
+ *      Author: Matt
+ */
+
+#include "RBELib/RBELib.h"
+#include "main.h"
+
+/**
+ * @brief Find the angle of the given potentiometer.
+ * @param  pot The pot to check.
+ * @return angle Angle of the potentiometer.
+ *
+ * @todo Calculate the angle using the ADC reading.
+ */
+int potAngle(int pot){
+	return 0;
+}
+
+/**
+ * @brief Find the voltage value of the given potentiometer.
+ * @param  pot The pot to get the value of.
+ * @return volts Voltage of  potentiometer.
+ *
+ * @todo Convert the ADC value into a voltage in mV (so no floats needed).
+ */
+int potVolts(int pot){
+	return 0;
+}
