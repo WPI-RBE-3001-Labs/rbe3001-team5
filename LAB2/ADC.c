8b4285fc22232b1dc680aeb38a4f0b7955e61bf6
calum <calum@Noggin>
2017-01-29 15:58:13 -0500

    Commented and fixed issue with initADC

--- LAB2/ADC.c
+++ LAB2/ADC.c
@@ -23,34 +23,29 @@ void initADC(int channel){
 	PRR0 = 0x00;
 
 
-	//Enable ADC
-	// ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-
-	ADCSRA =     (1 << ADEN) |
-				 (0 << ADSC) |
-				 (0 << ADATE)|
-				 (0 << ADIF) |
-				 (0 << ADIE) |
-				 (1 << ADPS2)|
-				 (1 << ADPS1)|
-				 (1 << ADPS0);
-	//set ref voltage and multiplexer port 7
-
+    //Info can be found on page 322 of ATmega644P data sheet
+	ADCSRA =     (1 << ADEN) | // Enable the ADC
+				 (0 << ADSC) | // Starts conversion, set to 0 initially
+				 (0 << ADATE)| // AutoTriggering Disabled
+				 (0 << ADIF) | // Interrupt flag not used s disabled
+				 (0 << ADIE) | // Interrupts disabled
+				 (1 << ADPS2)| // Table 25-5 Division factor: 111 -> 128
+				 (1 << ADPS1)| // Table 25-5 ""
+				 (1 << ADPS0); // Table 25-5 ""
 
 
 	//Info can be found on page 319 of ATmega644P data sheet
-	ADMUX =     (0 << REFS1)| // Table 25-3
-				(1 << REFS0)| // Table 25-3
+	ADMUX =     (0 << REFS1)| // Table 25-3 Select reference voltage
+				(1 << REFS0)| // Table 25-3 ""
 				(1 << ADLAR)| // 1 to left Adjust data, 0 to right Adjust data
-				(0 << MUX4) | // Table 25-4
-				(0 << MUX3) | // Table 25-4
-				(0 << MUX2) | // Table 25-4
-				(0 << MUX1) | // Table 25-4
-				(0 << MUX0);  // Table 25-4
-	if(channel <= 7 && channel >= 0) // select ADC channel, overwrites MUX, MUX2, MUX1 if valid channel selection
-		ADMUX |= channel;
-	//ADC in Free Mode
-	//ADCSRB &= 0b11111000
+				(0 << MUX4) | // Table 25-4 Select ADC mode
+				(0 << MUX3) | // Table 25-4 ""
+				(0 << MUX2) | // Table 25-4 Select ADC Channel
+				(0 << MUX1) | // Table 25-4 ""
+				(0 << MUX0);  // Table 25-4 ""
+
+	if(channel <= 7 && channel >= 0) // select ADC channel, overwrites MUX2, MUX1, and MUX0 if valid channel selection
+		ADMUX &= 0b11111000 + channel;
 
 }
 
