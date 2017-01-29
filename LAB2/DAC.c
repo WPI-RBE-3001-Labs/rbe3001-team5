abb06ea573cfff0fc8db9243c685718b9e67d36c
mmlamare <mmlamare@gmail.com>
2017-01-29 17:15:49 -0500

    good DAC code is in

--- LAB2/DAC.c
+++ LAB2/DAC.c
@@ -18,26 +18,45 @@
  *
  * http://cds.linear.com/docs/en/datasheet/2634fc.pdf
  */
-void setDAC(int DACn, int SPIVal){
-	//Set enable pin for DAC
-	DDRDbits._P4 = OUTPUT; //Set D4 GPIO as output
-	PINDbits._P4 = 0; //pull to 0 to enable chip transmission
+void setDAC(int DACn, int SPIval){
+	 //We need to send out 3 "packages" for configuring the DAC
+	  unsigned char package1 = 0;
+	  unsigned char package2 = 0;
+	  unsigned char package3 = 0;
+	  //Temp value for creating the packages from the SPI value
+	  unsigned long temp = 0;
 
-	//load in 32 bits of Data:
-	//4 bit command - write to input register n
-	char MSBDAC = 0x0;
-	//4 bit DAC address - just use DACn as the LS bits
-	//send first byte through SPI to DAC:
-	spiTransceive((4 << MSBDAC) | DACn);
+	  //Package 1 is:
+	  //Write to and update (power up) DAC register n
+	  package1 = (0x30 | DACn);
 
-	//Transmit:
-	//we want 12 data bits:
-	spiTransceive((char)(SPIVal >> 4)); //top 8 of 12 bits
-	spiTransceive((char)(8 << SPIVal));
-	//second half of this byte is ignored by chip
+	  //If value is over our max, cap it at 4095
+	  if(SPIval >= 4096) SPIval = 4095;
 
-	//Turn off DAC Transmission:
-	PINDbits._P4 = 1;
+	  //Copy the SPIval
+	  temp = SPIval;
+	  //Keep the upper 8 bits (drops bottom 4)
+	  temp = temp >> 4;
+	  //This is our second package
+	  package2 = temp;
+
+	  //Keep only the bottom 4 bits
+	  temp = SPIval & 0x000F;
+	  temp = temp << 4;
+	  //This will be our final package
+	  //The bottom 4 bits are don't cares
+	  package3 = temp;
+
+	  //Assert the DAC
+	  DAC_SS = 0;
+	  //transmit the 3 packets
+	  spiTransceive(package1);
+	  spiTransceive(package2);
+	  spiTransceive(package3);
+	  //Toggle the SS line to load and execute
+	  DAC_SS = 1;
+	  DAC_SS = 0;
+	  DAC_SS = 1;
 }
 
 
