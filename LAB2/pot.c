f98c9c96b696dc690f9b609ce7c51a2ca670b1c9
mmlamare <mmlamare@gmail.com>
2017-01-30 15:21:34 -0500

    added in first TF

--- LAB2/pot.c
+++ LAB2/pot.c
@@ -9,17 +9,17 @@
 #include "main.h"
 
 #define TICKS_TO_VOLTS 200 //todo get the actual value
-#define VOLTS_TO_ANGLE 0.25 //todo get actual value
+#define TICKS_TO_DEGREE 4.41 //todo get actual value
 
 /**
  * @brief Find the angle of the given potentiometer.
  * @param  pot The pot to check.
  * @return angle Angle of the potentiometer.
  *
- * @todo Calculate the angle using the ADC reading.
+ * Calculate the angle using the ADC reading.
  */
-float potAngle(int pot){
-	return pot * TICKS_TO_VOLTS * VOLTS_TO_ANGLE;
+int potAngle(int pot){
+	return (int)(pot * TICKS_TO_DEGREE);
 }
 
 /**
@@ -27,10 +27,10 @@ float potAngle(int pot){
  * @param  pot The pot to get the value of.
  * @return volts Voltage of  potentiometer.
  *
- * @todo Convert the ADC value into a voltage in mV (so no floats needed).
+ *Convert the ADC value into a voltage in mV (so no floats needed).
  */
 
 
-float potVolts(int pot){
-	return pot * TICKS_TO_VOLTS;
+int potVolts(int pot){
+	return (int)(pot * TICKS_TO_VOLTS);
 }
