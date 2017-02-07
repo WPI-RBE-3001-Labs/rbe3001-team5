04e660e719a28a6f0c1a6fc75dc5c77375081c68
mmlamare <mmlamare@gmail.com>
2017-02-07 16:43:46 -0500

    tried to make it work

--- LAB2/main.c
+++ LAB2/main.c
@@ -21,16 +21,14 @@ int main(){
 	//initialize USART 1 for transmission to Putty
 	debugUSARTInit(115200);
 
-	initTimer(0,0,0);
+	//initTimer(0,0,0);
 
 	//TODO Make sure that the DAC is ON/OFF properly
 	//disable DAC for doing potentiometer stuff
-	//DAC_SS = 1;
+	DAC_SS = 1;
 
 	//TODO Interchange the correct part of the lab
-	readCurrentSense();
-
-
+	sawtoothWave();
 
 	return 1;
 } /* End main */
@@ -78,56 +76,71 @@ void logPot2(){
 }//end LogPot()
 
 //globals for t
-int counter0 = 100;
+int counter0 = 0;
 signed char counter0Dir = 1;
-int counter1 = 100;
-signed char counter1Dir = 1;
+int counter1 = 4095;
+signed char counter1Dir = -1;
 BOOL flag = 1;
 char LEDON = 0;
 
 void sawtoothWave(){
-	DDRA = (1 << DDA5);
+	//DDRA = (1 << DDA5);
 	//disable IO port B
-	DDRB = 0;
+	//DDRB = 0;
 	//setup the SPI bus
 	initSPI();
 	//will output waves between channels 0 and 1
 	//setup Timer
-	initTimer(0,0,0);
+	//initTimer(0,0,0);
 	//setCompValue(0, 16);
 
 	while(1){
-		if(flag){
-			PORTAbits._P5 = LEDON;
-			//printf("DAC Val: %d \n\r", counter0);
-			//printf("DAC Val: %d \n\r", counter1);
-			setDAC(0, counter0);
-			setDAC(1, counter1);
-			flag = 0;
-		}
-			//PINAbits._P7 = 0;
+//		if(flag){
+//			PORTAbits._P5 = LEDON;
+//			//printf("DAC Val: %d \n\r", counter0);
+//			//printf("DAC Val: %d \n\r", counter1);
+//			setDAC(0, counter0);
+//			setDAC(1, counter1);
+//			flag = 0;
+//		}
+//			//PINAbits._P7 = 0;
+		setDAC(0, counter0);
+		setDAC(1, counter1);
 
+		if (counter0 > 4095 || counter0 < 0 ){ //If it is out of bounds
+			counter0Dir *= -1; //change direction
+		}
+		counter0 += counter0Dir;
+
+		//Counter 1
+		if (counter1 > 4095 || counter1 < 0 ){ //If it is out of bounds
+			counter1Dir *= -1; //change direction
+		}
+		counter1 += counter1Dir;
+
+		printf("DAC0 %d ", (int) counter0); printf(", \n\r");
+		printf("DAC1 %d ", (int) counter1); printf(", \n\r");
 	}
 }
 //ISR for timer
-ISR(TIMER0_OVF_vect){
-	//Counter 0
-	if (counter0 > 4095 || counter0 < 0 ){ //If it is out of bounds
-		counter0Dir *= -1; //change direction
-	}
-	counter0 += counter0Dir;
-
-	//Counter 1
-	if (counter1 > 4095 || counter1 < 0 ){ //If it is out of bounds
-		counter1Dir *= -1; //change direction
-	}
-	counter1 += counter1Dir;
-	//printf("DAC Val: %d \n\r", flag);
-	flag = 1;
-	LEDON = 1 - LEDON;
-	TIFR0 = (1 << TOV0);
-	//sei();
-}
+//ISR(TIMER0_OVF_vect){
+//	//Counter 0
+//	if (counter0 > 4095 || counter0 < 0 ){ //If it is out of bounds
+//		counter0Dir *= -1; //change direction
+//	}
+//	counter0 += counter0Dir;
+//
+//	//Counter 1
+//	if (counter1 > 4095 || counter1 < 0 ){ //If it is out of bounds
+//		counter1Dir *= -1; //change direction
+//	}
+//	counter1 += counter1Dir;
+//	//printf("DAC Val: %d \n\r", flag);
+//	flag = 1;
+//	LEDON = 1 - LEDON;
+//	TIFR0 = (1 << TOV0);
+//	//sei();
+//}
 
 
 //TODO
@@ -137,6 +150,8 @@ void driveMotors(){
 
 #define CurrentSense0Port 0
 #define CurrentSense1Port 1
+#define noLoadCurrent 553
+#define voltageScaler 2
 //readCurrentSense() funciton
 void readCurrentSense(){
 	//Initialize ADC first
@@ -144,7 +159,9 @@ void readCurrentSense(){
 	struct Current current0 = {0,0,0};
 	while(1){
 		current0.ADCVal = getADC(CurrentSense0Port);
-		printf("Current Sensor %d ", (int) current0.ADCVal); printf(",");
+		current0.ADCVal -= noLoadCurrent;
+		current0.voltage *= voltageScaler;
+		printf("Current Sensor %d ", (int) current0.ADCVal); printf(", \n\r");
 	}
 }
 
