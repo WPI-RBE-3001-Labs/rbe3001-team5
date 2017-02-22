ac08eab65ce5236bd0df770567a917fe0569b9ac
mmlamare <mmlamare@gmail.com>
2017-02-22 11:15:48 -0500

    initial commit

---
+++ LAB4/ir.c
@@ -0,0 +1,28 @@
+/*
+ * ir.c
+ *
+ *  Created on: Feb 22, 2017
+ *      Author: Matt
+ */
+
+#include "RBELib/RBELib.h"
+#include "main.h"
+
+//setup IR function
+//setups up the analog IR values
+//TODO implement this
+void setupIR(char port){
+
+}
+
+//read IR sensor, return ADC value
+//TODO implement this
+int getIRValue(char port){
+	return 0;
+}
+
+//converts and IR ADC value to a distance in millimeters
+//TODO
+int adcToMillimeters(int adc){
+	return 0;
+}
