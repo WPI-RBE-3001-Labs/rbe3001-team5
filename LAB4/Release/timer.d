d8a3e9226ffe31d3dcb88e309e0ce852590f4e86
crbriggs <crbriggs.wpi.edu>
2017-02-22 11:31:22 -0500

    Added Kinematics

--- LAB4/Release/timer.d
+++ LAB4/Release/timer.d
@@ -1,46 +1,46 @@
-timer.o timer.o: ../timer.c \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/RBELib.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/ADC.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/DAC.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/Debug.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/motors.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/USARTDebug.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/timer.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/Periph.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/pot.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/PID.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/reg_structs.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/ports.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/SPI.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/SetServo.h \
-  C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/SlaveSelects.h
+timer.d timer.o: ../timer.c \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/RBELib.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/ADC.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/DAC.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/Debug.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/motors.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/USARTDebug.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/timer.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/Periph.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/pot.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/PID.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/reg_structs.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/ports.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/SPI.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/SetServo.h \
+  C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/SlaveSelects.h
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/RBELib.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/RBELib.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/ADC.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/ADC.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/DAC.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/DAC.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/Debug.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/Debug.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/motors.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/motors.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/USARTDebug.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/USARTDebug.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/timer.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/timer.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/Periph.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/Periph.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/pot.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/pot.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/PID.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/PID.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/reg_structs.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/reg_structs.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/ports.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/ports.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/SPI.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/SPI.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/SetServo.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/SetServo.h:
 
-C:\Users\Matt\git\RBE3001\rbelib\RBELib\include/RBELib/SlaveSelects.h:
+C:\Users\crbriggs\git\rbe3001-team5\RBELib\include/RBELib/SlaveSelects.h:
